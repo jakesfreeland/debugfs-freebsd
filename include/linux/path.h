abd76bba910a85b8cb799893ab1deaa1ed8fe09b
Jake Freeland <jakesfreeland@gmail.com>
2022-06-28 12:08:15 -0500

    moved header files into proper linux subdir

--- include/linux/path.h
+++ include/linux/path.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2022 Jake Freeland
+ * Copyright (c) 2022 Jake Freeland <jfree@freebsd.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
